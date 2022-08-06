a5c3bc6dfbe1962c837e962edb8865808a64f80d
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-08-06 10:23:03 -0300

    bru

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+let butt = document.getElementById('buti');
+let msg = document.getElementById('msg');
+
 function clicked(){
-	document.getElementById('buti').classList.remove('noclick');
-	document.getElementById('buti').classList.add('click');
-	setTimeout(()=>{
-		document.getElementById('buti').classList.remove('click');
-		document.getElementById('buti').classList.add('noclick');
-	}, 200);
-}
+	let num = document.getElementById('numi');
+	num = Number(num.value);
+	if (num == 0 || num == ''){
+		window.alert('Please, type a number!');
+	}else{
+		contar();
+	};
+};
+
+function contar(){ // Não funcionando.
+	let num = document.getElementById('numi');
+	num = Number(num.value);
+	butt.remove();
+	if(num <= 0){
+		setTimeout(()=>{
+			n--;
+			contar(num);
+		}, num * 1000);
+	}else{
+		window.alert('Finished!');
+	};
+};
