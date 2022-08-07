a63a8f38be60a56c74452944262a1ae51f809145
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-08-07 15:28:16 -0300

    Semifinished

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 let butt = document.getElementById('buti');
 let msg = document.getElementById('msg');
+let entr = document.getElementById('entr');
 
 function clicked(){
 	let num = document.getElementById('numi');
 	num = Number(num.value);
-	if (num == 0 || num == ''){
-		window.alert('Please, type a number!');
+	if (num <= 0 || num == ''){
+		msg.style.padding = '5px';
+		msg.style.border = '3px solid #3E5B74';
+		msg.innerHTML = `Please, type a number!`;
 	}else{
 		contar();
 	};
-};
-
-function contar(){ // Não funcionando.
-	let num = document.getElementById('numi');
-	num = Number(num.value);
-	butt.remove();
-	if(num <= 0){
-		setTimeout(()=>{
-			n--;
-			contar(num);
-		}, num * 1000);
-	}else{
-		window.alert('Finished!');
+	
+	function contar(){
+		msg.innerHTML = `${num}`;
+		msg.style.padding = '5px';
+		msg.style.border = '3px solid #3E5B74';
+		butt.setAttribute('onclick', '');
+		if(num + 1 > 0){
+			setTimeout(()=>{
+				num--;
+				contar();
+			}, 1 * 1000);
+		}else{
+			butt.setAttribute('onclick', 'clicked()');
+			msg.innerHTML = `Finished!`;
+		};
 	};
 };
